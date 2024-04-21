968ff76cd280115bb44a9753730dec19f8d81028
recyclebini <trivicni@yahoo.com>
2024-04-21 03:20:20 +0200

    Update Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,6 +4,7 @@ const Logo: React.FC<{ src: string; alt: string }> = ({ src, alt }) => (
   <img
     src={src}
     alt={alt}
+    onClick={() => window.location.reload()} // Add onClick event to refresh the page
     className="h-32 -mr-10 w-auto object-contain shrink-0 self-center" // Aligning SVG with the center of flex items
   />
 );
@@ -12,7 +13,7 @@ const Logo: React.FC<{ src: string; alt: string }> = ({ src, alt }) => (
 
 const Navbar: React.FC = () => (
   <header className="flex items-center justify-between p-4 bg-white w-full h-20 shadow-md"> {/* Fixed height to Navbar */}
-    <div className="flex items-center"> {/* Flex container for logo and text */}
+    <div className="flex items-center cursor-pointer"> {/* Flex container for logo and text */}
       <Logo src="/logo_maxsize.svg" alt="LunchBunch logo" />
       <h1 className="ml-2 text-3xl font-righteous text-black">BunchLunch</h1> {/* Using custom font class */}
     </div>
